7b19a77f4e9a150317e2370b1c52c25b69b74c63
Andrés <32048923+andresftr@users.noreply.github.com>
2019-04-11 10:39:29 -0500

    Revert "Feature/welcome"

--- app/controllers/welcome_controller.rb
+++
@@ -1,6 +0,0 @@
-# frozen_string_literal: true
-
-# Welcome Controller
-class WelcomeController < ApplicationController
-  def index; end
-end
--- app/models/application_record.rb
+++
@@ -1,6 +0,0 @@
-# frozen_string_literal: true
-
-# Application Record
-class ApplicationRecord < ActiveRecord::Base
-  self.abstract_class = true
-end
