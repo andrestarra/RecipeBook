855ab6729faf717820efeb8b55b0a5eba63f3816
AndresFTR <afelipe0113@gmail.com>
2019-04-12 16:29:20 -0500

    Model and controllers from steps created

---
+++ app/controllers/steps_controller.rb
@@ -0,0 +1,50 @@
+class StepsController < ApplicationController
+  def index
+    @steps = Step.all  
+  end
+
+  def show
+    @step = Step.find(params[:id])
+  end
+
+  def new
+    @step = Step.new
+  end
+
+  def edit
+    @step = Step.find(params[:id])
+  end
+
+  def create
+    @step = Step.new(step_params)
+
+    if @step.save
+      redirect_to @step
+    else
+      render 'new'
+    end
+  end
+
+  def update
+    @step = Step.find(params[:id])
+    
+    if @step.update(step_params)
+      redirect_to @step
+    else
+      render 'edit'
+    end
+  end
+  
+  def destroy
+    @step = Step.find(params[:id])
+    @step.destroy
+
+    redirect_to steps_path
+  end
+
+  private
+
+  def step_params
+    params.require(:step).permit(:operation, :schedule_time, :comment)
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_04_12_160054) do
+ActiveRecord::Schema.define(version: 2019_04_12_205347) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -24,6 +24,14 @@ ActiveRecord::Schema.define(version: 2019_04_12_160054) do
 
   create_table "menus", force: :cascade do |t|
     t.string "type"
+    t.string "comment"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "steps", force: :cascade do |t|
+    t.string "operation"
+    t.time "schedule_time"
     t.string "comment"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
