be8dd8854e00f4e7be1852e43fd62e4353305d40
AndresFTR <afelipe0113@gmail.com>
2019-04-12 10:38:58 -0500

    Test of the ingredient model

--- spec/models/ingredient_spec.rb
+++ spec/models/ingredient_spec.rb
@@ -1,5 +1,8 @@
 require 'rails_helper'
 
 RSpec.describe Ingredient, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it { should validate_presence_of(:name) }
+  it { should validate_length_of(:name).is_at_least(3) }
+  it { should validate_length_of(:name).is_at_most(20) }
+  it { should validate_length_of(:calories).is_at_most(5) }
 end
