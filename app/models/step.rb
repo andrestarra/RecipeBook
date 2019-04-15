47353bf4282a31bcd77f935cf4bf60519c39ff78
AndresFTR <afelipe0113@gmail.com>
2019-04-15 15:15:49 -0500

    Fix typo in Step model

--- app/models/step.rb
+++ app/models/step.rb
@@ -3,5 +3,5 @@
 # Step model
 class Step < ApplicationRecord
   validates :operation, presence: true, length: { minimum: 3 }
-  validates :expected_minutes, lenght: { maximum: 5 }
+  validates :expected_minutes, length: { maximum: 5 }
 end
