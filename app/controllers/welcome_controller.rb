cdc62a3683547e3986aed1a386c41faaa4a6911b
AndresFTR <afelipe0113@gmail.com>
2019-05-09 14:45:11 -0500

    Restrict the show view welcome only if unlogged

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -2,11 +2,22 @@
 
 # Welcome Controller
 class WelcomeController < ApplicationController
+  before_action :only_unlogged, only: [:show]
+
   def index
     @recipes = Recipe.order(:created_at).reverse_order.last(5)
   end
 
   def show
     @recipe = Recipe.find(params[:id])
+  end
+
+  private
+
+  def only_unlogged
+    if user_signed_in?
+      flash[:error] = "You haven't access to this url"
+      redirect_to root_path
+    end
   end
 end
