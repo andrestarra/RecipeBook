74a54e130a3ee6a71d6cc9d0279b8c65fd966279
AndresFTR <afelipe0113@gmail.com>
2019-05-23 14:07:29 -0500

    Add the mailer config in production with sendgrid

--- .bashrc
+++ .bashrc
@@ -1,2 +1,5 @@
 export SENDMAIL_PASSWORD=password
 export SENDMAIL_USERNAME=recipe@book.com
+
+export SENDGRID_USERNAME=recipe@book.com
+export SENDGRID_PASSWORD=password
--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,14 @@ require_relative 'application'
 
 # Initialize the Rails application.
 Rails.application.initialize!
+
+# Setting ActionMailer to setting to SendGrid's servers
+ActionMailer::Base.smtp_settings = {
+  user_name: ENV['SENDGRID_USERNAME'],
+  password: ENV['SENDGRID_PASSWORD'],
+  domain: 'herokuapp.com',
+  address: 'smtp.sendgrid.net',
+  port: 587,
+  authentication: :plain,
+  enable_starttls_auto: true
+}
